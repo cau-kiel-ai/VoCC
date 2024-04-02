6cef98127f0889a5226245e79985b8e1012ce986
ywoelker <ywoelker@gmail.com>
2024-04-02 13:32:07 +0200

    Extending VoCC to 3D-VoCC. - Adding the depth of the particles in scope of the GeoInformatica submission.

--- scripts/experiment_utils.py
+++ scripts/experiment_utils.py
@@ -4,6 +4,38 @@ import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 import numpy as np
 
+depth_layers = np.array([4.7679895e-01, 1.5707064e+00, 2.8554180e+00, 4.3355064e+00,
+       6.0156703e+00, 7.9007444e+00, 9.9957113e+00, 1.2305715e+01,
+       1.4836073e+01, 1.7592300e+01, 2.0580124e+01, 2.3805506e+01,
+       2.7274673e+01, 3.0994144e+01, 3.4970764e+01, 3.9211739e+01,
+       4.3724689e+01, 4.8517693e+01, 5.3599339e+01, 5.8978794e+01,
+       6.4665886e+01, 7.0671165e+01, 7.7006004e+01, 8.3682709e+01,
+       9.0714615e+01, 9.8116249e+01, 1.0590342e+02, 1.1409344e+02,
+       1.2270523e+02, 1.3175960e+02, 1.4127936e+02, 1.5128963e+02,
+       1.6181805e+02, 1.7289508e+02, 1.8455423e+02, 1.9683240e+02,
+       2.0977017e+02, 2.2341211e+02, 2.3780708e+02, 2.5300858e+02,
+       2.6907489e+02, 2.8606949e+02, 3.0406100e+02, 3.2312344e+02,
+       3.4333621e+02, 3.6478378e+02, 3.8755551e+02, 4.1174518e+02,
+       4.3745007e+02, 4.6477032e+02, 4.9380743e+02, 5.2466309e+02,
+       5.5743713e+02, 5.9222620e+02, 6.2912128e+02, 6.6820563e+02,
+       7.0955292e+02, 7.5322528e+02, 7.9927124e+02, 8.4772443e+02,
+       8.9860266e+02, 9.5190729e+02, 1.0076231e+03, 1.0657189e+03,
+       1.1261479e+03, 1.1888505e+03, 1.2537540e+03, 1.3207762e+03,
+       1.3898268e+03, 1.4608092e+03, 1.5336232e+03, 1.6081664e+03,
+       1.6843364e+03, 1.7620319e+03, 1.8411539e+03, 1.9216071e+03,
+       2.0033002e+03, 2.0861470e+03, 2.1700664e+03, 2.2549822e+03,
+       2.3408240e+03, 2.4275269e+03, 2.5150303e+03, 2.6032795e+03,
+       2.6922241e+03, 2.7818181e+03, 2.8720193e+03, 2.9627896e+03,
+       3.0540945e+03, 3.1459023e+03, 3.2381846e+03, 3.3309153e+03,
+       3.4240706e+03, 3.5176292e+03, 3.6115713e+03, 3.7058794e+03,
+       3.8005366e+03, 3.8955283e+03, 3.9908408e+03, 4.0864612e+03,
+       4.1823779e+03, 4.2785801e+03, 4.3750576e+03, 4.4718013e+03,
+       4.5688027e+03, 4.6660532e+03, 4.7635449e+03, 4.8612715e+03,
+       4.9592251e+03, 5.0573999e+03, 5.1557891e+03, 5.2543877e+03,
+       5.3531895e+03, 5.4521890e+03, 5.5513809e+03, 5.6507607e+03,
+       5.7503232e+03, 5.8500645e+03, 5.9499790e+03, 6.0500630e+03],
+      dtype=np.float32)
+
 def create_synthetic_outlier_set(seed):
     np.random.seed(seed)    
     image = np.zeros(shape= (500, 300))
@@ -199,11 +231,16 @@ def calc_rates(true_labels, predicted_labels):
     return {'TP': TP, 'TN': TN, 'FP': FP, 'FN': FN}
 
 
-def create_real_world_image(coordinates, labels, output_name):
+def create_real_world_image(coordinates, labels, output_name, text = False, ax = None, font_size = 16):
     unique_labels = set(labels)
     colors = plt.cm.gist_ncar(np.linspace(0, 1, len(unique_labels)))
-    fig = plt.figure(figsize= (10,5))
-    ax = fig.add_subplot(1,1,1, projection = PlateCarree())
+
+    if ax is None:
+        fig = plt.figure(figsize= (10,5))
+        ax = fig.add_subplot(1,1,1, projection = PlateCarree())
+    else:
+        output_name = None # Disable the saving because this is only part of a bigger plot
+    
     ax.add_feature(COASTLINE)
     ax.add_feature(LAND, facecolor = 'darkgrey')
 
@@ -212,11 +249,13 @@ def create_real_world_image(coordinates, labels, output_name):
         if k == -1:
             ax.plot(xy[:, 0], xy[:, 1], '.',color='grey', markersize=0.1)
         if k > -1:
-            ax.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col, markeredgecolor = 'k', markersize=6,zorder=2)
+            ax.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col, markeredgecolor = 'k', markersize=6,zorder=2, alpha = .3)
+            if text:
+                ax.text(xy[:, 0].mean(), xy[:, 1].mean(), str(int(k)), fontsize = font_size, c = 'w')
 
     gl = ax.gridlines(draw_labels=True, dms=False, x_inline=False, y_inline=False)
     gl.top_labels, gl.right_labels = False, False
-    gl.xlabel_style, gl.ylabel_style = {'fontsize': 16}, {'fontsize': 16}
+    gl.xlabel_style, gl.ylabel_style = {'fontsize': font_size}, {'fontsize': font_size}
 
     lat_min = -45
     lat_max = -31
@@ -224,4 +263,7 @@ def create_real_world_image(coordinates, labels, output_name):
     lon_max = 35
     ax.set_extent([lon_min, lon_max, lat_min, lat_max])
 
-    plt.savefig(f'../fig/{output_name}.png', dpi = 300)
+    if output_name is not None:
+        plt.savefig(f'../fig/{output_name}.png', dpi = 300)
+
+    return ax
--- src/VoCC/vocc_clustering.py
+++ src/VoCC/vocc_clustering.py
@@ -42,7 +42,9 @@ class VortexCorrelationClustering(ClusterMixin, BaseEstimator):
         sectors,
         circle_coverage_rate,
         qth_threshold,
-        min_points
+        min_points,
+        depth_boundaries = None,
+        cylindrical = False
     ):
     
         super().__init__()
@@ -52,6 +54,8 @@ class VortexCorrelationClustering(ClusterMixin, BaseEstimator):
         self.circle_coverage_rate = circle_coverage_rate
         self.qth_threshold = qth_threshold
         self.min_points = min_points
+        self.depth_boundaries = depth_boundaries
+        self.cylindrical = cylindrical
         self.vortices_ = None
         self.labels_ = None
 
@@ -71,12 +75,23 @@ class VortexCorrelationClustering(ClusterMixin, BaseEstimator):
             Returns a fitted instance of self.
         """
 
-        assert X.shape[1] == 4, 'The input array must be structured as (n_samples, 4) for the propoerties position X, position Y, movement X, movement Y along the second dimension'
+        assert X.shape[1] == 5, 'The input array must be structured as (n_samples, 5) for the propoerties position X, position Y, movement X, movement Y along the second dimension, and the Depth D'
 
-        X, Y, U, V = X.T
-        normal_vectors = _bin_image(X, Y, U, V, self.cell_size)
+        X, Y, U, V, D = X.T
 
-        accumulator = _create_parameter_space(normal_vectors, self.radii, self.sectors, self.circle_coverage_rate)
+        self.min_positions = np.array([np.min(X), np.min(Y)])
+
+        if self.depth_boundaries is None:
+            D_binned = np.ones_like(D, dtype = np.int32)
+        else:
+            D_binned = np.digitize(D, bins = self.depth_boundaries)
+
+        # Normalize the depth index to a 0-based index
+        D_binned = D_binned - D_binned.min()
+        self.D_binned = D_binned
+        normal_vectors = _bin_image(X, Y, U, V, D_binned, self.cell_size)
+
+        accumulator = _create_parameter_space(normal_vectors, self.radii, self.sectors, self.circle_coverage_rate, self.depth_boundaries, self.cylindrical)
         
         threshold = np.max(np.abs(np.quantile(accumulator, [1 - self.qth_threshold, self.qth_threshold])))
         vortices_direction_1 = _extract_vortices_from_parameter_space(accumulator, threshold, self.radii, 'a')
@@ -88,15 +103,40 @@ class VortexCorrelationClustering(ClusterMixin, BaseEstimator):
 
 
         labels = np.full_like(X, fill_value=-1)
-        particle_tree = KDTree(np.array([X,Y]).T)
-        for vortex_id, vortex in enumerate(vortices):
 
-            backprojected_positions = vortex.get_circle_positions() * self.cell_size + np.array([np.min(X), np.min(Y)])
-            backprojected_radii = vortex.get_circle_radii() * self.cell_size
 
-            influenced_position_indices = particle_tree.query_radius(backprojected_positions, backprojected_radii)
-            influenced_position_indices = np.concatenate(influenced_position_indices)
-            labels[influenced_position_indices] = vortex_id
+        for depth_index in range(D_binned.max() + 1):
+            mask = D_binned == depth_index
+            if np.sum(mask) < 1:
+                continue
+            X_d = X[mask]
+            Y_d = Y[mask]
+
+            mask_indices = np.atleast_1d(np.argwhere(mask).squeeze())
+
+            particle_tree = KDTree(np.array([X_d,Y_d]).T)
+            for vortex_id, vortex in enumerate(vortices):
+
+                backprojected_positions = vortex.get_circle_positions() * self.cell_size + np.array([np.min(X), np.min(Y)])
+                backprojected_radii = vortex.get_circle_radii() * self.cell_size
+
+                circle_depth_mask = vortex.pixels[:, 3] == depth_index
+
+                if np.sum(circle_depth_mask) < 1:
+                    continue
+
+                backprojected_positions = backprojected_positions[circle_depth_mask]
+                backprojected_radii = backprojected_radii[circle_depth_mask]
+
+                influenced_position_indices = particle_tree.query_radius(backprojected_positions, backprojected_radii)
+                influenced_position_indices = np.concatenate(influenced_position_indices)
+                
+                
+                
+                if self.cylindrical:
+                    labels[influenced_position_indices] = vortex_id
+                else:
+                    labels[mask_indices[influenced_position_indices]] = vortex_id
 
         self.labels_ = labels
         self.vortices_ = vortices
@@ -105,35 +145,64 @@ class VortexCorrelationClustering(ClusterMixin, BaseEstimator):
 
 
 class Vortex():
-    def __init__(self, x_bin: int, y_bin: int, radius: int, pixels: np.ndarray, flag):
+    def __init__(self, x_bin: int, y_bin: int, radius: int, pixels: np.ndarray, flag, depth_level):
         self.x_bin = x_bin 
         self.y_bin = y_bin
         self.radius = radius 
         self.pixels = pixels
         self.flag = flag
+        self.depths_level = depth_level
     
     def __repr__(self) -> str:
         return f'[{self.x_bin},{self.y_bin}] -> {self.radius}; #pixels {self.pixels.shape[0] if isinstance(self.pixels, np.ndarray) else len(self.pixels)}'
 
     def get_circle_positions(self):
-        return self.pixels[:, :2]
+        try:
+            pixel_positions = self.pixels[:, :2]
+        except:
+            print(self.pixels)
+            raise
+        return pixel_positions
+
     
     def get_circle_radii(self):
         return self.pixels[:, 2]
+    
+
+    def get_depth_layers(self):
+        return np.unique(self.pixels[:, 3])
 
 
-def _bin_image(X, Y, U, V, cell_size):
+def _bin_image(X, Y, U, V, D_binned, cell_size):
     bins_X = np.arange(np.min(X), np.max(X), cell_size)
     bins_Y = np.arange(np.min(Y), np.max(Y), cell_size)
 
     X_indices = np.digitize(X, bins = bins_X)
     Y_indices = np.digitize(Y, bins = bins_Y)
     
-    movement = np.zeros(shape = (bins_X.shape[0] +1, bins_Y.shape[0]+1, 2))
-    normal = np.zeros(shape = (bins_X.shape[0] +1, bins_Y.shape[0]+1, 2))
+    movement = np.zeros(shape = (bins_X.shape[0] +1, bins_Y.shape[0]+1, D_binned.max() + 1, 2))
+    normal = np.zeros(shape = (bins_X.shape[0] +1, bins_Y.shape[0]+1, D_binned.max() + 1, 2))
+
+    for d_i in range(D_binned.max() + 1):
+        mask = D_binned == d_i
+        U_d = U[mask]
+        V_d = V[mask]
+        X_d = X_indices[mask]
+        Y_d = Y_indices[mask]
+
+        for x_i, x in enumerate(bins_X):
+            for y_i, y in enumerate(bins_Y):
+                mask = (X_d == x_i) & (Y_d == y_i)
+                if np.sum(mask) < 1:
+                    continue
+                U_x_y = U_d[mask]
+                V_x_y = V_d[mask]
+                U_x_y = np.median(U_x_y)
+                V_x_y = np.median(V_x_y)
+
+                movement[x_i, y_i, d_i, 0] = U_x_y
+                movement[x_i, y_i, d_i, 1] = V_x_y
     
-    movement[X_indices, Y_indices, 0] = (U / cell_size).squeeze()
-    movement[X_indices, Y_indices, 1] = (V / cell_size).squeeze()
 
     # Normalize the length of the movement
     norm = np.linalg.norm(movement, axis = -1)[..., np.newaxis]
@@ -144,13 +213,13 @@ def _bin_image(X, Y, U, V, cell_size):
 
     return normal
 
-def _create_parameter_space(normal_vectors, radii, sectors, circle_coverage_rate):
+def _create_parameter_space(normal_vectors, radii, sectors, circle_coverage_rate, depth_boundaries, cylindrical):
     accumulator = []
     for radius in radii:
         XX, YY = np.meshgrid(np.arange(2 * radius +1), np.arange(2* radius +1 ))
 
         distances_from_center =(XX-radius)**2 + (YY-radius)**2
-        activation_mask = (distances_from_center < radius ** 2).astype(np.float)
+        activation_mask = (distances_from_center < radius ** 2).astype(np.float32)
         
 
         angle_mask = np.arctan2(XX-radius, YY-radius)
@@ -170,12 +239,19 @@ def _create_parameter_space(normal_vectors, radii, sectors, circle_coverage_rate
 
             ht_mask_x = activation_mask * activation_mask_sector * direction_mask_x 
             ht_mask_y = activation_mask * activation_mask_sector * direction_mask_y 
-            sector_vector_i = convolve(normal_vectors[..., 00], ht_mask_x) + convolve(normal_vectors[..., 1], ht_mask_y)
+            depth_dependent_convolution = []
+            for d_i in range(normal_vectors.shape[2]):
+                depth_dependent_convolution.append(convolve(normal_vectors[..., d_i, 0], ht_mask_x) + convolve(normal_vectors[..., d_i, 1], ht_mask_y))
+            
+            if cylindrical:
+                sector_vector_i = np.sum(np.array(depth_dependent_convolution), axis = 0, keepdims=True)
+            else:
+                sector_vector_i = np.array(depth_dependent_convolution)
 
             sector_accumulator.append(sector_vector_i)
         
         # qth-quantile selection of the sector vectors
-        sector_accumulator = np.array(sector_accumulator).reshape(sectors, normal_vectors.shape[0], normal_vectors.shape[1])
+        sector_accumulator = np.array(sector_accumulator).reshape(sectors, sector_accumulator[0].shape[0], normal_vectors.shape[0], normal_vectors.shape[1])
 
         positive_vlaues = np.quantile(np.where(sector_accumulator > 0, sector_accumulator, 0), 1 - circle_coverage_rate, axis = 0)
         negative_vlaues = np.quantile(np.where(sector_accumulator < 0, sector_accumulator, 0), circle_coverage_rate, axis = 0)
@@ -189,15 +265,17 @@ def _create_parameter_space(normal_vectors, radii, sectors, circle_coverage_rate
 def _extract_vortices_from_parameter_space(accumulator, threshold, radii, flag):
     vortices = []
 
-    accumulator_high_values = np.maximum(accumulator, np.full_like(accumulator, fill_value=threshold))
+    for depth_index in range(accumulator.shape[1]):
 
-    accumulator2d = np.max(accumulator_high_values, axis= 0)
-    accumulator2d_indices = np.argmax(accumulator_high_values, axis= 0)
+        accumulator_high_values = np.maximum(accumulator[:, depth_index], np.full_like(accumulator[:, depth_index], fill_value=threshold))
 
-    local_maxima = argrelmax(accumulator2d)
-    indices = np.array(local_maxima)
-    for index in indices.T:
-        vortices.append(Vortex(index[0],index[1], radii[accumulator2d_indices[index[0], index[1]]], [], flag))
+        accumulator2d = np.max(accumulator_high_values, axis= 0)
+        accumulator2d_indices = np.argmax(accumulator_high_values, axis= 0)
+
+        local_maxima = argrelmax(accumulator2d)
+        indices = np.array(local_maxima)
+        for index in indices.T:
+            vortices.append(Vortex(index[0],index[1], radii[accumulator2d_indices[index[0], index[1]]], np.empty(shape = (0,4)), flag, depth_index))
 
     return vortices
 
@@ -223,9 +301,9 @@ def _merge_votex_candidates(vortices, radii, rdbscan_min_points):
     if len(vortices) < 1:
         return []
 
-    X = np.empty(shape = (len(vortices), 3))
+    X = np.empty(shape = (len(vortices), 4))
     for vortex_i, vortex in enumerate(vortices):
-        X[vortex_i] = [vortex.x_bin, vortex.y_bin, vortex.radius]
+        X[vortex_i] = [vortex.x_bin, vortex.y_bin, vortex.radius, vortex.depths_level]
 
     labels = _radius_eps_DBSCAN(X, max(5,int(rdbscan_min_points * len(vortices))))
 
@@ -236,56 +314,86 @@ def _merge_votex_candidates(vortices, radii, rdbscan_min_points):
         mask = labels == l
 
         if np.sum(mask) == 1:
-            vortices_grouped.append(vortices[np.argwhere(mask).squeeze()])
+            vortex = vortices[np.argwhere(mask).squeeze()]
+            pixel = [[vortex.x_bin, vortex.y_bin, vortex.radius, vortex.depths_level]]
+            vortex.pixels = np.array(pixel).reshape(1, 4)
+            vortices_grouped.append(vortex)
         elif np.sum(mask) > 1:
             indices = np.argwhere(labels == l).squeeze()
 
+            if len(indices) < 1:
+                continue
+
             pixels = []
-            inner_x_y_r = np.empty(shape= (len(indices), 3))
+            inner_x_y_r = np.empty(shape= (len(indices), 4))
             for index_i, index in enumerate(indices):
                 vortex = vortices[index]
-                inner_x_y_r[index_i] = [vortex.x_bin, vortex.y_bin, vortex.radius]
+                inner_x_y_r[index_i] = [vortex.x_bin, vortex.y_bin, vortex.radius, vortex.depths_level]
                 pixels.append(vortex.pixels)
+
+            if len(pixels) < 1:
+                continue
+
 
             new_x_bin = np.around(np.mean(inner_x_y_r[:, 0]), decimals= 0)
             new_y_bin = np.around(np.mean(inner_x_y_r[:, 1]), decimals= 0)
 
-            vortex_group = Vortex(new_x_bin, new_y_bin, np.max(inner_x_y_r[:, 2]), inner_x_y_r, vortex.flag)
+            vortex_group = Vortex(new_x_bin, new_y_bin, np.max(inner_x_y_r[:, 2]), inner_x_y_r, vortex.flag, np.atleast_1d(np.unique(inner_x_y_r[:, 3])))
             vortices_grouped.append(vortex_group)
 
     return vortices_grouped
 
 def _radius_eps_DBSCAN(X: np.ndarray, min_samples : int):
     """
-    X is an array (n, 3) with x, y, and radius.
+    X is an array (n, 4) with x, y, radius, and depth_index
     """
-    assert X.shape[1] == 3, "Please use three dimensions for the radius DBSCAN"
+    assert X.shape[1] == 4, "Please use three dimensions for the radius DBSCAN"
 
-    tree = KDTree(X[:, :2])
     neighborhoods = np.empty(shape = (X.shape[0]), dtype = object)
+    for depth_index in np.unique(X[:, 3]):
+        depth_mask_outer = np.abs(X[:, 3] - depth_index) <= 1
+        depth_mask_inner = X[:, 3] == depth_index
+        depth_mask_outer_indices = np.atleast_1d(np.argwhere(depth_mask_outer).squeeze())
 
-    for r in np.unique(X[:, 2])[::-1]:
-        r_mask = X[:, 2] == r
-        neighbors_per_r = tree.query_radius(X[r_mask, :2],r)
 
-        """
-        # We dont need this, there is something else (probably in dbscan_inner that makes it bidirectional)
-        for i in range(neighbors_per_r.shape[0]):
-            print(np.argwhere(r_mask).squeeze())
-            
-            looking_for = np.argwhere(r_mask).squeeze()
-            if neighbors_per_r.shape[0] > 1:
-                looking_for = looking_for[i]
-            additional_neighbors = []
-            for j in range(neighborhoods.shape[0]):
-                if neighborhoods[j] is None:
-                    continue
-                if looking_for in neighborhoods[j]:
-                    additional_neighbors.append(j)
-            neighbors_per_r[i] = np.concatenate([neighbors_per_r[i], additional_neighbors]).astype(np.intp)
-        """
+        X_outer_depth = X[depth_mask_outer]
+        X_inner_depth = X[depth_mask_inner]
+        
 
-        neighborhoods[r_mask] = neighbors_per_r
+        tree = KDTree(X_outer_depth[:, :2])
+        
+        neighborhoods_in_depth_layer = np.empty(shape = (depth_mask_inner.sum()), dtype = object)
+        
+        for r in np.unique(X_inner_depth[:, 2])[::-1]:
+            r_mask = X_inner_depth[:, 2] == r
+            neighbors_per_r = tree.query_radius(X_inner_depth[r_mask, :2],r)
+
+            # map the indices back to the original array
+            neighbors_per_r = [depth_mask_outer_indices[neighbors] for neighbors in neighbors_per_r]
+
+            """
+            # We dont need this, there is something else (probably in dbscan_inner that makes it bidirectional)
+            for i in range(neighbors_per_r.shape[0]):
+                print(np.argwhere(r_mask).squeeze())
+                
+                looking_for = np.argwhere(r_mask).squeeze()
+                if neighbors_per_r.shape[0] > 1:
+                    looking_for = looking_for[i]
+                additional_neighbors = []
+                for j in range(neighborhoods.shape[0]):
+                    if neighborhoods[j] is None:
+                        continue
+                    if looking_for in neighborhoods[j]:
+                        additional_neighbors.append(j)
+                neighbors_per_r[i] = np.concatenate([neighbors_per_r[i], additional_neighbors]).astype(np.intp)
+            """
+
+            neighborhoods_in_depth_layer[r_mask] = np.array(neighbors_per_r + [None], dtype=object)[:-1]
+        
+        # Find all vortices that are at the same x, y location but are in a different depth layer
+        # Assign the neighbor indices to the same index in the neighborhoods array
+        
+        neighborhoods[depth_mask_inner] = neighborhoods_in_depth_layer
 
     n_neighbors = np.array([len(neighbors) for neighbors in neighborhoods])
     labels = np.full(X.shape[0], -1, dtype=np.intp)
